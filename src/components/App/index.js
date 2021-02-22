5ef76067067760636902c883fccf8a60257ae09e
fulviocanducci <fulviocanducci@hotmail.com>
2021-02-22 16:03:02 -0300

    1 file modified

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,22 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
   const [results, setResults] = useState([]);
   useEffect(() => {
-    fetch('https://randomuser.me/api/?results=6&inc=name,picture')
+    fetch("https://randomuser.me/api/?results=6&inc=name,picture")
       .then(result => result.json())
       .then(result => setResults(result.results))
       .catch(err => console.log(err));
   }, []);
   return (
     <div>
-      {results && results.map((item, index) => 
-        (<div key={index}>
-          <img src={item.picture.thumbnail} alt="" />  {item.name.title} {item.name.first} {item.name.last}
-        </div>)
-      )}      
+      {results &&
+        results.map((item, index) => (
+          <div key={index}>
+            <img src={item.picture.thumbnail} alt="" />
+            {item.name.title} {item.name.first} {item.name.last}
+          </div>
+        ))}
     </div>
   );
 }
