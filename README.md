50df36818b7fb26087dfa5f2c02572319e1287d2
fulviocanducci <fulviocanducci@hotmail.com>
2021-02-22 15:49:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fwtqrv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fwtqrv)
---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+import React, {useEffect, useState} from "react";
+
+export default function App() {
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=6&inc=name,picture')
+      .then(result => result.json())
+      .then(result => setResults(result.results))
+      .catch(err => console.log(err));
+  }, []);
+  return (
+    <div>
+      {results && results.map((item, index) => 
+        (<div key={index}>
+          <img src={item.picture.thumbnail} alt="" />  {item.name.title} {item.name.first} {item.name.last}
+        </div>)
+      )}      
+    </div>
+  );
+}
